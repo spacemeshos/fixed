70f7d34592e32d95ce211142616bb981dd84e17a
Alexey Sudachen <alexey@sudachen.name>
2020-07-09 01:46:28 -0400

    updated exp tests

--- exp_test.go
+++ exp_test.go
@@ -28,6 +28,15 @@ func TestRange_Exp240(t *testing.T) {
 	}()
 }
 
+func TestRange_ExpEpowMax(t *testing.T) {
+	func() {
+		defer func() { recover() }()
+		x := Fixed{int64(1) << (fracBits + len(epow) + 1)}
+		Exp(x)
+		t.Errorf("exp(%v) must panic by overflow", x)
+	}()
+}
+
 func BenchmarkFixed_Exp(b *testing.B) {
 	for i := 1; i < 1000000; i++ {
 		Result = Exp(Fixed{int64(i % 130000)})
