faca3df5d335e8bc4f803c752eb48d49f3fae4b7
Alexey Sudachen <alexey@sudachen.name>
2020-07-10 12:06:29 -0400

    Refactored Exp function

--- exp.go
+++ exp.go
@@ -5,22 +5,24 @@ const eValue = int64(2718281) * (1 << fracBits) / 1000000
 // Exp calculates e^x
 func Exp(x Fixed) Fixed {
 	if x.int64 < 0 {
-		// a = -x, e^a = 2^(log2(E)*a) = 2^(-i+f) = 2^f>>i
+		// when x < 0
+		// => e^x = 2^(log2(E)*x) = 2^(-i+f) = 2^f>>i = 2^expApprox[f] >> i
+		// 0 <= f < 4096
 		n := Fixed{log2_E}.Mul(x.Neg()).int64
 		f := oneValue - n&fracMask
 		n += oneValue
 		return Fixed{int64(pow2Approx[f]) >> (n >> fracBits)}
 	}
-	// e^x = e^(i+f) = e^i*e^f => iexp(x)*fexp(x)
-	return iexp(x).Mul(Fixed{int64(expApprox[x.int64&fracMask])})
-}
-
-// iexp calculates e^x for a positive integer part
-func iexp(x Fixed) Fixed {
 	if x.int64 > 144796 {
 		panic(ErrOverflow)
 	}
-	n := x.int64 >> fracBits
+	// e^x = e^(i+f) = e^i*e^f => e^i*expApprox[f]
+	// 0 <= f < 4096
+	return iexp(x.int64 >> fracBits).Mul(Fixed{int64(expApprox[x.int64&fracMask])})
+}
+
+// iexp calculates e^n for a positive integer part
+func iexp(n int64) Fixed {
 	r := Fixed{oneValue}
 	if n == 0 {
 		return r
