a583a0f3a1d95b56a11d0225c7bd753c9bc2f49b
Alexey Sudachen <alexey@sudachen.name>
2020-08-15 17:20:34 -0400

    add bincdf overflow test

--- bincdf_test.go
+++ bincdf_test.go
@@ -2,6 +2,7 @@ package fixed
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -11,4 +12,25 @@ func TestFixed_BinCDF(t *testing.T) {
 
 func TestFixed_BinCDF1(t *testing.T) {
 	fmt.Printf("%.10f\n", BinCDF(New(340), From(0.82), New(250)).Float())
+}
+
+func TestFixed_BinCDF2(t *testing.T) {
+	require.NotPanics(t, func() {
+		BinCDF(New(1471), From(.05), New(2))
+	})
+	require.NotPanics(t, func() {
+		BinCDF(New(1472), From(.05), New(2))
+	})
+	require.PanicsWithValue(t, ErrOverflow, func() {
+		BinCDF(New(6471), From(.05), New(2))
+	})
+	require.PanicsWithValue(t, ErrOverflow, func() {
+		BinCDF(New(6472), From(.05), New(2))
+	})
+	require.PanicsWithValue(t, ErrOverflow, func() {
+		BinCDF(New(20480), From(.05), New(2))
+	})
+	require.PanicsWithValue(t, ErrOverflow, func() {
+		BinCDF(New(20481), From(.05), New(2))
+	})
 }
